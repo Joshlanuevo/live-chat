2014d157026e6b222c46806b28bb73858b349687
chris <chris@gmail.com>
2024-01-09 10:07:21 +0800

    修复bug

--- AppPluginIm/src/main/java/com/ym/plugin/ChatImConfig.kt
+++ AppPluginIm/src/main/java/com/ym/plugin/ChatImConfig.kt
@@ -5,12 +5,12 @@ package com.ym.plugin
  */
 class ChatImConfig : AppConfig() {
     //版本code
-    override var versionCode = 115
+    override var versionCode = 100
 
     //版本名称，开发的版本号固定0.0.0
     //当前测试环境版本 0.3.8
     //当前生产环境版本 1.0.3
-    override var versionName = "1.0.8"
+    override var versionName = "1.0.0"
 
     //包名
     override var applicationId = "com.youliao.chat"
